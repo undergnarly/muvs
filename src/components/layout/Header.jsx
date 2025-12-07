2c65f6a7e322c3041d0fff5740bd4809e1ed6448
Nikita Antimonov <Nikita.Antimonov@msk.action.ru>
2025-12-07 22:09:11 +0800

    Fix: Add color inversion for logo and menu button text

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import Logo from './Logo';
 import StaggeredMenu from './StaggeredMenu';
 import './Header.css';
 
 const Header = () => {
     return (
-        <header className="header">
-            <div className="header-content">
-                <Logo />
-                <StaggeredMenu />
-            </div>
-        </header>
+        <StaggeredMenu />
     );
 };
 
